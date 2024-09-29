5e6a9b1fb41b03114f2d36e7e55a395f7fd83014
Riddhesh Desai <riddheshdesai56@gmail.com>
2024-09-29 18:55:24 +0530

    fix: Refactor test cases and code as foloww TDD

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -2,7 +2,27 @@ const stringCal = require("../utils/stringCalculator");
 
 // Describing all test cases regarding string calculation
 describe("String calculator", () => {
-    test("Should return 0 for emtpty string", () => {
-        expect(stringCal("")).toBe(0);
-    })
+  test("Should return 0 for emtpty string", () => {
+    expect(stringCal("")).toBe(0);
+  });
+  test("Should return number itself when input is single number", () => {
+    expect(stringCal("1")).toBe(1);
+  });
+  test("Should return the sum of two comma seperated numbers", () => {
+    expect(stringCal("1,5")).toBe(6);
+  });
+  test("Should return the sum of multiple comma seperated numbers", () => {
+    expect(stringCal("5,7,8,12")).toBe(32);
+  });
+  test("Should handle new line with comma seperator to sum numbers", () => {
+    expect(stringCal("1\n2,3")).toBe(6);
+  });
+  test("should handle different delimiters", () => {
+    expect(stringCal("//;\n1;2")).toBe(3);
+  });
+  test("should throw an exception for negative numbers", () => {
+    expect(() => stringCal("1,-2,3,-5")).toThrow(
+      "negative numbers not allowed : -2, -5"
+    );
+  });
 });
--- utils/stringCalculator.js
+++ utils/stringCalculator.js
@@ -1,6 +1,21 @@
-// function return string calculations 
+// function return string calculations
 function stringCalculator(string) {
-    if(string === "") return 0;
+  let inputString = string;
+  if (string === "") return 0;
+  let delimiter = /[\n,]/;
+  if (string.startsWith("//")) {
+    const parts = string.split("\n");
+    delimiter = new RegExp(`${parts[0][2]}`);
+    inputString = parts[1];
+  }
+
+  const numbbers = inputString.split(delimiter).map(Number);
+
+  const negetiveNum = numbbers.filter((number) => number < 0);
+  if (negetiveNum.length > 0) {
+    throw new Error(`negative numbers not allowed : ${negetiveNum.join(", ")}`);
+  }
+  return numbbers.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = stringCalculator;
